960a3da546012205e760badce95e97fde119388d
Tony Blake <avblake225@gmail.com>
2016-01-28 17:55:20 +0000

    Added first pub image and functionality

--- app/src/main/java/com/tonyblake/dublinpubfinder/PubDetailsScreen.java
+++ app/src/main/java/com/tonyblake/dublinpubfinder/PubDetailsScreen.java
@@ -2,8 +2,11 @@ package com.tonyblake.dublinpubfinder;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.database.Cursor;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class PubDetailsScreen extends Activity{
@@ -12,7 +15,10 @@ public class PubDetailsScreen extends Activity{
     private String address;
     private String directions;
 
+    private Context context;
+
     TextView tv_name;
+    ImageView iv_pub;
     TextView tv_address;
     TextView tv_directions;
 
@@ -26,7 +32,10 @@ public class PubDetailsScreen extends Activity{
         address = savedInstanceState.getString("address");
         directions = savedInstanceState.getString("directions");
 
+        context = this;
+
         tv_name = (TextView)findViewById(R.id.tv_name);
+        iv_pub = (ImageView)findViewById(R.id.iv_pub);
         tv_address = (TextView)findViewById(R.id.tv_address);
         tv_directions = (TextView)findViewById(R.id.tv_directions);
     }
@@ -36,6 +45,14 @@ public class PubDetailsScreen extends Activity{
         super.onResume();
 
         tv_name.setText(name);
+
+        Drawable pub_pic = null;
+
+        if(context.getString(R.string.the_temple_bar).equals(tv_name.getText())){
+            pub_pic = context.getResources().getDrawable(R.drawable.temple_bar);
+        }
+
+        iv_pub.setImageDrawable(pub_pic);
         tv_address.setText(address);
         tv_directions.setText(directions);
     }
