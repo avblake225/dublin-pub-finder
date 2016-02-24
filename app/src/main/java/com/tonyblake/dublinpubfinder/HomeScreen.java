95ccf8377adf4f761a6454669f099035cb7933b2
Tony Blake <avblake225@gmail.com>
2016-02-24 17:30:21 +0000

    Updated UI. On entering app user is now presented with series of questions.

---
+++ app/src/main/java/com/tonyblake/dublinpubfinder/HomeScreen.java
@@ -0,0 +1,109 @@
+package com.tonyblake.dublinpubfinder;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+public class HomeScreen extends AppCompatActivity {
+
+    private Context context;
+    private DBManager dbManager;
+    private RadioButton traditional_pub, modern_pub;
+    private RadioButton north_side, south_side;
+    private RadioButton yes_to_live_music, no_to_live_music;
+    private RadioButton yes_to_late_pub, no_to_late_pub;
+    private Button btn_findpub;
+
+    private String name, address;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+
+        context = this;
+
+        dbManager = new DBManager(this);
+
+        traditional_pub =(RadioButton)findViewById(R.id.radiobutton_traditional);
+        modern_pub =(RadioButton)findViewById(R.id.radiobutton_modern);
+
+        north_side =(RadioButton)findViewById(R.id.radiobutton_northside);
+        south_side =(RadioButton)findViewById(R.id.radiobutton_southside);
+
+        yes_to_live_music =(RadioButton)findViewById(R.id.radiobutton_yestolivemusic);
+        no_to_live_music =(RadioButton)findViewById(R.id.radiobutton_notolivemusic);
+
+        yes_to_late_pub =(RadioButton)findViewById(R.id.radiobutton_yestolatepub);
+        no_to_late_pub =(RadioButton)findViewById(R.id.radiobutton_notolatepub);
+
+        btn_findpub = (Button) findViewById(R.id.btn_findpub);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        name = null;
+        address = null;
+
+        btn_findpub.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                boolean traditional_pub_chosen = traditional_pub.isChecked();
+                boolean modern_pub_chosen = modern_pub.isChecked();
+
+                boolean north_side_chosen = north_side.isChecked();
+                boolean south_side_chosen = south_side.isChecked();
+
+                boolean yes_to_live_music_chosen = yes_to_live_music.isChecked();
+                boolean no_to_live_music_chosen = no_to_live_music.isChecked();
+
+                boolean yes_to_late_pub_chosen = yes_to_late_pub.isChecked();
+                boolean no_to_late_pub_chosen = no_to_late_pub.isChecked();
+
+                String dummy = "dummy string";
+
+                /*try {
+                    Cursor res = dbManager.getPub(name_chosen);
+
+                    while (res.moveToNext()) {
+                        name = res.getString(1);
+                        address = res.getString(2);
+                        break;
+                    }
+
+                    if (name != null) {
+
+                        launchPubDetailsScreen();
+
+                    } else {
+                        showToastMessage(context.getString(R.string.pubnotfound));
+                    }
+                } catch (Exception e) {
+                    showToastMessage(context.getString(R.string.errorfindingpub));
+                }*/
+            }
+        });
+    }
+
+    private void launchPubDetailsScreen() {
+        Intent intent = new Intent(this, PubDetailsScreen.class);
+        intent.putExtra("name", name);
+        intent.putExtra("address", address);
+        startActivity(intent);
+    }
+
+    private void showToastMessage(CharSequence text) {
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+}
--- app/src/main/java/com/tonyblake/dublinpubfinder/MainActivity.java
+++ app/src/main/java/com/tonyblake/dublinpubfinder/MainActivity.java
@@ -2,25 +2,15 @@ package com.tonyblake.dublinpubfinder;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    Context context;
-    DBManager dbManager;
-    AutoCompleteTextView tv_qwhatpub;
-    String[] pubs;
-    Button btn_findpub;
-    String name_entered, name_chosen, name_retrieved;
-    String address;
+    private Context context;
+    Button btn_enter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,85 +19,27 @@ public class MainActivity extends AppCompatActivity {
 
         context = this;
 
-        dbManager = new DBManager(this);
-
-        tv_qwhatpub = (AutoCompleteTextView) findViewById(R.id.tv_qwhatpub);
-
-        pubs = dbManager.getPubNames();
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, pubs);
-        tv_qwhatpub.setThreshold(1);
-        tv_qwhatpub.setAdapter(adapter);
-
-        btn_findpub = (Button) findViewById(R.id.btn_findpub);
+        btn_enter = (Button) findViewById(R.id.btn_enter);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        name_entered = null;
-        name_chosen = null;
-        name_retrieved = null;
-
-        System.out.println("Number of pubs: " + DBManager.pub_names.length);
-
-        tv_qwhatpub.setText("");
-
-        tv_qwhatpub.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long rowId) {
-
-                name_chosen = (String) parent.getItemAtPosition(position);
-            }
-        });
-
-        btn_findpub.setOnClickListener(new View.OnClickListener() {
+        btn_enter.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
 
-                name_entered = tv_qwhatpub.getText().toString();
+                launchHomeScreen();
 
-                if ("".equals(name_entered)) {
-                    showToastMessage(context.getString(R.string.enter_a_pub));
-                } else {
-                    try {
-                        Cursor res = dbManager.getPub(name_chosen);
-
-                        while (res.moveToNext()) {
-                            name_retrieved = res.getString(1);
-                            address = res.getString(2);
-                            break;
-                        }
-
-                        if (name_retrieved != null) {
-
-                            launchPubDetailsScreen();
-
-                        } else {
-                            showToastMessage(context.getString(R.string.pubnotfound));
-                        }
-                    } catch (Exception e) {
-                        showToastMessage(context.getString(R.string.errorfindingpub));
-                    }
-                }
             }
         });
     }
 
-    private void launchPubDetailsScreen() {
-        Intent intent = new Intent(this, PubDetailsScreen.class);
-        intent.putExtra("name", name_retrieved);
-        intent.putExtra("address", address);
+    private void launchHomeScreen(){
+        Intent intent = new Intent(this, HomeScreen.class);
         startActivity(intent);
-    }
-
-    private void showToastMessage(CharSequence text) {
-        int duration = Toast.LENGTH_SHORT;
-        Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
     }
 
     @Override
