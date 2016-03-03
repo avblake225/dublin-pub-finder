84f17bddc940fafc8fe4f0bd9ee18f28c24b7821
Tony Blake <avblake225@gmail.com>
2016-03-03 15:06:50 +0000

    Created thread to handle front screen (no longer a button press)

--- app/src/main/java/com/tonyblake/dublinpubfinder/MainActivity.java
+++ app/src/main/java/com/tonyblake/dublinpubfinder/MainActivity.java
@@ -1,40 +1,35 @@
 package com.tonyblake.dublinpubfinder;
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Context context;
-    Button btn_enter;
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        context = this;
-
-        btn_enter = (Button) findViewById(R.id.btn_enter);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
 
-        btn_enter.setOnClickListener(new View.OnClickListener() {
+        new Thread(new Runnable() {
 
-            @Override
-            public void onClick(View v) {
+            public void run() {
 
-                launchHomeScreen();
-
+                try{
+                    Thread.sleep(3000);
+                    launchHomeScreen();
+                }
+                catch(InterruptedException e){
+                    System.out.println("Thread exception");
+                }
             }
-        });
+        }).start();
     }
 
     private void launchHomeScreen(){
